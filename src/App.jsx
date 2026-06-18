44bcf2025b250b902aaa75e3a970db29cee26c80
ishida-g <rayonorigin@gmail.com>
2026-06-18 21:25:49 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="title">title</h1>
+    <label htmlFor="name">name:</label>
+    <input id="name" type="text" placeholder="input name" />
+
+    <label htmlFor="test">test:</label>
+    <input id="test" type="checkbox"/>
+    </>
+  )
+}
+
+export default App
