79d236f08f376eb8f7e9caf4df6d7d02c345517e
ishida-g <rayonorigin@gmail.com>
2026-07-14 21:39:39 +0900

    empty

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
 
-
+function Profile({name, age, isStudent, hometown}){
+  return (
+    <>
+      <h2>{name}</h2>
+      <p>年齢：{age}</p>
+      <p>{isStudent ? '学生' : '社会人'}</p>
+      <p>出身地：{hometown}</p>
+    </>
+  )
+}
 
 function App() {
 
-  const isGreen = false;
   return (
     <>
-    <p className={isGreen ? 'green' : 'red'}>サンプルテキスト</p>
-    {/* <p className='counter'>サンプルテキスト</p> */}
+      <Profile name="田中太郎" age={20} isStudent={true} hometown="東京"/>
+      <Profile name="aaa" age={18} isStudent={false} hometown="大阪"/>
     </>
   )
 }
