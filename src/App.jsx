525f4393ee54201b84bede081f555a8bb36d694d
ishida-g <rayonorigin@gmail.com>
2026-07-25 21:46:57 +0900

    empty

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
 
-function Profile({name, age, isStudent, hometown}){
+function Profile({name, age, isStudent, hometown, children}){
   return (
     <>
       <h2>{name}</h2>
       <p>年齢：{age}</p>
       <p>{isStudent ? '学生' : '社会人'}</p>
       <p>出身地：{hometown}</p>
+      {children}
     </>
   )
 }
@@ -15,7 +16,9 @@ function App() {
 
   return (
     <>
-      <Profile name="田中太郎" age={20} isStudent={true} hometown="東京"/>
+      <Profile name="田中太郎" age={20} isStudent={true} hometown="東京">
+        <p>趣味：プログラミング</p>
+      </Profile>
       <Profile name="aaa" age={18} isStudent={false} hometown="大阪"/>
     </>
   )
