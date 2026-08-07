1f2b9c9f7fb9f0500b2eccf58ee6371efd555753
ishida-g <rayonorigin@gmail.com>
2026-08-07 21:53:21 +0900

    empty

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import './App.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+
 
 function App() {
-  console.log('レンダリング');
   const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    console.log(`countが${count}に変わりました`);
+  }, [count]);
 
   return (
     <>
-      <p>現在のカウント: {count}</p>
-      <button onClick={() => setCount(count + 1)}>カウントアップ</button>
+      <div>
+        <p>現在のカウント: {count}</p>
+        <button onClick={() => setCount(count + 1)}>カウントアップ</button>
+      </div>
+
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="テキストを入力"
+        />
+        <p>入力: {text}</p>
+      </div>
     </>
   )
 }
