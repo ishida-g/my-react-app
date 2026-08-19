c958ac03983540a6ef0892cea43d4947dfc43fe5
ishida-g <rayonorigin@gmail.com>
2026-08-19 21:36:08 +0900

    empty

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
 import './App.css';
-import {useState, useEffect} from 'react';
-
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState('');
-
-  useEffect(() => {
-    console.log(`countが${count}に変わりました`);
-  }, [count]);
+  const productNames = ['ノートPC', 'マウス', 'キーボード', 'タブレット'];
 
   return (
-    <>
-      <div>
-        <p>現在のカウント: {count}</p>
-        <button onClick={() => setCount(count + 1)}>カウントアップ</button>
-      </div>
-
-      <div>
-        <input
-          type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="テキストを入力"
-        />
-        <p>入力: {text}</p>
-      </div>
-    </>
+    <ul>
+      {productNames.map((productName) => (
+        <li key={productName}>{productName}</li>
+      ))}
+    </ul>
   )
 }
 
