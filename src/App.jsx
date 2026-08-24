c04d2a8294bb686a3d97a7fddf70590d012511df
ishida-g <rayonorigin@gmail.com>
2026-08-24 22:20:46 +0900

    empty

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
 
 function App() {
-  const productNames = ['ノートPC', 'マウス', 'キーボード', 'タブレット'];
+  const products = [
+    {id: 1, name: 'ノートPC', price: 120000, category: 'pc'},
+    {id: 2, name: 'マウス', price: 3000, category: '周辺機器'},
+    {id: 3, name: 'キーボード', price: 8000, category: '周辺機器'},
+    {id: 4, name: 'タブレット', price: 50000, category: 'pc'},
+  ];
 
   return (
     <ul>
-      {productNames.map((productName) => (
-        <li key={productName}>{productName}</li>
+      {products
+        .filter((product) => product.category === '周辺機器')
+        .map((product) => (
+        <li key={product.id}>
+          {product.name}: ¥{product.price}
+        </li>
       ))}
     </ul>
   )
